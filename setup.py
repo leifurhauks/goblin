9031f34e7fc7e508efbbbe82771bbaf7e3c9552c
Jonathan Senecal <jsenecal@zerofail.com>
2016-03-03 14:09:28 -0500

    Updated setup.py long_description

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-import sys
 from setuptools import setup, find_packages
-
 
 version = open('goblin/VERSION', 'r').readline().strip()
 
-
 develop_requires = ['Sphinx==1.3.5',
-    'tornado==4.3',
-    'factory-boy==2.6.0',
-    'gremlinclient==0.1.9',
-    'newrelic==2.60.0.46',
-    'nose==1.3.7',
-    'pyformance==0.3.2',
-    'pyparsing==2.1.0',
-    'pytz==2015.7',
-    'six==1.10.0',
-    'sphinx-rtd-theme==0.1.9',
-    'tox==2.3.1',
-    'Twisted==15.5.0',
-    'watchdog==0.8.3']
+                    'tornado==4.3',
+                    'factory-boy==2.6.0',
+                    'gremlinclient==0.1.9',
+                    'newrelic==2.60.0.46',
+                    'nose==1.3.7',
+                    'pyformance==0.3.2',
+                    'pyparsing==2.1.0',
+                    'pytz==2015.7',
+                    'six==1.10.0',
+                    'sphinx-rtd-theme==0.1.9',
+                    'tox==2.3.1',
+                    'Twisted==15.5.0',
+                    'watchdog==0.8.3']
 
 long_desc = """
 Object-Graph Mapper (OGM) for the TinkerPop 3 Gremlin Server
@@ -26,6 +23,14 @@ Object-Graph Mapper (OGM) for the TinkerPop 3 Gremlin Server
 `Documentation <https://github.com/ZEROFAIL/goblin>`_
 
 `Report a Bug <https://github.com/ZEROFAIL/goblin/issues>`_
+
+Goblin uses @davebshow gremlinclient for asynchronous websocket based communication with the Gremlin Server,
+and is therefore designed to be multi-platform, allowing the user to choose between Tornado, Trollius, or Asyncio.
+
+It aims to provide full support for all TinkerPop3 enabled graph databases;
+however, it is currently only tested against Titan:db 1.x.
+
+This project is under active development.
 """
 
 setup(
