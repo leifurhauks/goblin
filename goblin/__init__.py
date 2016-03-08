3cc67640c239de509fe67b10ce2d8ca3c83d5c1b
davebshow <davebshow@gmail.com>
2016-03-08 14:29:23 -0500

    added MANIFEST to include groovy files in dist

--- goblin/__init__.py
+++ goblin/__init__.py
@@ -1,7 +1,4 @@
 import os
 
 __goblin_version_path__ = os.path.realpath(__file__ + '/../VERSION')
-try:
-    __version__ = open(__goblin_version_path__, 'r').readline().strip()
-except:
-    pass
+__version__ = open(__goblin_version_path__, 'r').readline().strip()
